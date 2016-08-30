1a5574113c938e5c7a7e30cbc322859f5f19d202
Yuan Lunchuan <799652013@qq.com>
2016-08-30 14:37:29 +0800

    1, finish huaxi doctor crawl.

--- huaxi_doctor/spiders/HuaXiDoctorSpider.py
+++ huaxi_doctor/spiders/HuaXiDoctorSpider.py
@@ -1,9 +1,9 @@
 import scrapy
+from huaxi_doctor.items import HuaxiDoctorItem
 
 class HuaXiDoctorSpider(scrapy.Spider):
   name = 'huaxi_doctor_spider'
   allowed_domains = ['www.cd120.com']
-  # http: // www.cd120.com / findDoctorListByRegService.jspx?pageNo = 2 & regCode = 33
   start_urls = [ ]
   for i in [1, 2, 3, 4, 5]:
     new_url = "http://www.cd120.com/findDoctorListByRegService.jspx?pageNo="+str(i)+"&regCode=33"
@@ -13,11 +13,19 @@ class HuaXiDoctorSpider(scrapy.Spider):
 
   def parse(self, response):
     filename = 'huaxi_doctors'
-    with open(filename, 'wb') as f:
-      doctors = response.xpath("//tr[@id='ret']")
-      for doctor in doctors:
-        f = open('huaxi_doctor_result.txt', 'a', encoding='utf-8')
-        f.writelines("医生名字: " + doctor.xpath("td[2]/span/a/i/text()").extract()[0])
-        f.writelines("    " + doctor.xpath("td[3]/span/text()").extract()[0])
-        f.writelines("    " + doctor.xpath("td[4]/span/em/text()").extract()[0] + "\n")
-      f.close()
+    doctors = response.xpath("//tr[@id='ret']")
+    for doctor in doctors:
+       item = HuaxiDoctorItem()
+       item['name'] = doctor.xpath("td[2]/span/a/i/text()").extract()[0]
+       item['level'] = doctor.xpath("td[3]/span/text()").extract()[0]
+       item['major'] = doctor.xpath("td[4]/span/em/text()").extract()[0]
+       yield item
+
+    # with open(filename, 'wb') as f:
+    #   doctors = response.xpath("//tr[@id='ret']")
+    #   for doctor in doctors:
+    #     f = open('huaxi_doctor_result.txt', 'a', encoding='utf-8')
+    #     f.writelines("医生名字: " + doctor.xpath("td[2]/span/a/i/text()").extract()[0])
+    #     f.writelines("    " + doctor.xpath("td[3]/span/text()").extract()[0])
+    #     f.writelines("    " + doctor.xpath("td[4]/span/em/text()").extract()[0] + "\n")
+    #   f.close()
